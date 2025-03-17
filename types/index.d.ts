bfbce95ba72d472d9b1521b325a2cd69c1ab95de
seif1125 <seifammar1125@gmail.com>
2025-03-17 23:57:01 +0200

    Add new interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,3 +6,14 @@ declare interface HeaderBoxProps {
   subtext: string;
   user?: string;
 }
+
+declare interface TotalBalanceBoxProps {
+  accounts?:string[];
+  totalBanks: number;
+  currentBalance: number;
+}
+
+declare interface DoughnutChartProps {
+  accounts:string[];
+}
+
