c3ac79b9b92d7be53f2cdca36a34a1a67c8f42ac
seif1125 <seifammar1125@gmail.com>
2025-03-19 23:56:38 +0200

    Add hover menu style properity

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -111,7 +111,7 @@ const config = {
   		},
   		backgroundImage: {
   			'bank-gradient': 'linear-gradient(90deg, #0179FE 0%, #4893FF 100%)',
-  			
+  			'bank-gradient-hover-effect':'linear-gradient(90deg, #0179fe78 0%, #4893ff7d 100%)',
   			'bank-green-gradient': 'linear-gradient(90deg, #01797A 0%, #489399 100%)'
   		},
   		boxShadow: {
