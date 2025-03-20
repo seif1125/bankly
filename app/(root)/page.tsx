4989166ca52b81f39f0a09c2cce4be02a85542ee
seif1125 <seifammar1125@gmail.com>
2025-03-21 01:14:44 +0200

    Implement Home Dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSideBar from '@/components/RightSideBar'
 const dashboard = () => {
-  const user= {'firstname':'seif'}
+  const user= {'firstName':'seif','lastName':'amr','email':'seifammar1125@gmail.com'}
   return (
-    <section className='home'> <div className='home-content '>
+    <section className='home'> 
+    
+    <div className='home-content '>
+      
     <HeaderBox 
     title='welcome'
     type='greeting' 
@@ -17,7 +21,37 @@ const dashboard = () => {
      currentBalance={1234.35}
 
      />
-    </div></section>
+
+    </div>
+    <RightSideBar 
+     user={user}
+      transactions={[{id:'1',amount:1234.35,type:'deposit',date:'2021-09-01'}]} 
+      banks={[
+        {
+          id: '1',
+          name: 'AIB',
+          branch: 'Lagos',
+          accountNumber: '1234567890',
+          balance: 1234.35,
+          cardHolder: 'John Doe',
+          cardNumber: '1234 5678 9012 3456',
+          expiryDate: '12/27'
+        },
+        {
+          id: '2',
+          name: 'GTBank',
+          branch: 'Abuja',
+          accountNumber: '0987654321',
+          balance: 4321.65,
+          cardHolder: 'John Doe',
+          cardNumber: '9876 5432 1098 7654',
+          expiryDate: '08/26'
+        }
+      ]}
+      
+     
+     />
+    </section>
    
   )
 }
