c42fe3e96b49547c2629dc964d8a8318525cf37c
seif1125 <seifammar1125@gmail.com>
2025-03-25 00:20:14 +0200

    Move interfaces to types

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,15 +1,8 @@
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Control, FieldValues, Path } from "react-hook-form";
+import { CustomInputProps } from "@/types";
+import {  FieldValues} from "react-hook-form";
 
-interface CustomInputProps<T extends FieldValues> {
-  name: Path<T>;
-  label: string;
-  placeholder?: string;
-  type?: string;
-  control: Control<T>;
-  fullWidth?: boolean;
-}
 
 const CustomInput = <T extends FieldValues>({
   name,
@@ -28,7 +21,7 @@ const CustomInput = <T extends FieldValues>({
           <FormItem>
             <FormLabel className="form-label">{label}</FormLabel>
             <FormControl>
-              <Input className="form-item" placeholder={placeholder} type={type} {...field} />
+              <Input className="form-item" placeholder={placeholder}  {...field} type={type} />
             </FormControl>
             <FormMessage />
           </FormItem>
