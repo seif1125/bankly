3132462e6f57c66956af5e3f298881deb40beaa9
seif1125 <seifammar1125@gmail.com>
2025-03-25 00:17:44 +0200

    Add image tolayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 
 
 
@@ -13,8 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <main>
+   <main className="flex justify-between min-h-screen w-full">
     {children}
+    <div className="auth-asset">
+       <div>
+        <Image src={'./icons/auth-image.svg'} alt="auth image" width={500} height={500} />
+       </div>
+    </div>
    </main>
   );
 }
